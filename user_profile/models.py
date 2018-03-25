45893732dcfdb31af9bd4f30ac8e66ff8a972c4d
jibin jose <jibinjosepez@gmail.com>
2018-03-26 00:10:19 +0530

    added task api

--- user_profile/models.py
+++ user_profile/models.py
@@ -6,11 +6,13 @@ class JobInfo(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	job_title = models.CharField(max_length=255)
 	job_url = models.TextField()
+	stage = models.CharField(max_length=255)
+	deadline = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now=True)
 
 
 class Task(models.Model):
 	action = models.CharField(max_length=255)
-	action_date = models.DateTimeField()
+	action_date = models.CharField(max_length=255)
 	job = models.ForeignKey(JobInfo, related_name='tasks', on_delete=models.CASCADE)
 
